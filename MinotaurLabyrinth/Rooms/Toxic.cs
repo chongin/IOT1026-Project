d5402af1696048939ffce67c6f658e0b6b6ad924
ngchongin <ngchongin@gmail.com>
2023-06-14 23:19:58 -0400

    add test case

--- MinotaurLabyrinth/Rooms/Toxic.cs
+++ MinotaurLabyrinth/Rooms/Toxic.cs
@@ -1,7 +1,7 @@
 ﻿namespace MinotaurLabyrinth
 {
     /// <summary>
-    /// Represents a Toxic room, which make the hero can play at most 3 steps.
+    /// Represents a Toxic room, which make the hero can play at most {n} steps.
     /// </summary>
     public class Toxic : Room
     {
@@ -26,37 +26,15 @@
             {
                 ConsoleHelper.WriteLine("You walk into the Toxic room, and you have to play a dice game.If you win, nothing happen, If you lost, you would get punishment!", ConsoleColor.Red);
                 ConsoleHelper.WriteLine("Choose large, small, after that will dice 3 dices, and calculate the result.", ConsoleColor.Gray);
-                bool smallFlag = Console.ReadLine() switch
-                {
-                    "small" => true,
-                    "large" => false,
-                    _ => false
-                };
+                bool smallFlag = GetUserSelectOption();
 
-                bool winFlag = false;
-                int point = Dice3Dices();
-                if (smallFlag)
+                if (PlayGame(smallFlag))
                 {
-                    winFlag = point <= 11;
+                    HandleWinGame();
                 }
                 else
                 {
-                    winFlag = point >= 12;
-                }
-
-                if (winFlag)
-                {
-                    ConsoleHelper.WriteLine("Congrat you win the dice game, nothing will happen.", ConsoleColor.Yellow);
-                }
-                else
-                {
-                    ConsoleHelper.WriteLine($"Be careful, you can play at most {MAX_PLAY_STEPS} steps from now.", ConsoleColor.Red);
-                    hero.IsPoisoned = true;
-                    hero.StillCanPlaySteps = MAX_PLAY_STEPS;
-
-                    //set the callback for the game to call when game ended.
-                    //because this room already finish, but the game keep going.
-                    hero.Callback = HandleCauseDeth;
+                    HandleLostGame(hero);
                 }
 
                 IsActive = false;
@@ -98,21 +76,64 @@
             return false;
         }
 
-        private int Dice3Dices()
+        //need to set virtual and protected, then the Test can override this funtion return value.
+        protected virtual int Dice3Dices()
         {
             int number = 0;
             for (int i = 0; i < 3; ++i)
             {
                 number += RandomNumberGenerator.Next(1, 6);
             }
-
+            ConsoleHelper.WriteLine($"You dice: {number} when you are playing the dice game!", ConsoleColor.Yellow);
             return number;
+        }
+
+        protected virtual bool GetUserSelectOption()
+        {
+            return Console.ReadLine() switch
+            {
+                "small" => true,
+                "large" => false,
+                _ => false
+            };
+        }
+
+        protected virtual void HandleWinGame()
+        {
+            ConsoleHelper.WriteLine("Congrat you win the dice game, nothing will happen.", ConsoleColor.Yellow);
+        }
+
+        protected virtual void HandleLostGame(Hero hero)
+        {
+            ConsoleHelper.WriteLine($"Be careful, you can play at most {MAX_PLAY_STEPS} steps from now.", ConsoleColor.Red);
+            hero.IsPoisoned = true;
+            hero.StillCanPlaySteps = MAX_PLAY_STEPS;
+
+            //set the callback for the game to call when game ended.
+            //because this room already finish, but the game keep going.
+            //I want this feature for that can affect the game flow, not only the room close then nothing happen.
+            hero.Callback = HandleCauseDeth;
+        }
+
+        private bool PlayGame(bool selectSmall)
+        {
+            bool winFlag = false;
+            int point = Dice3Dices();
+            if (selectSmall)
+            {
+                winFlag = point <= 10;
+            }
+            else
+            {
+                winFlag = point >= 11;
+            }
+            return winFlag;
         }
 
         //handle call back when the game ended.
         public static void HandleCauseDeth(Hero hero)
         {
-            hero.Kill("You have gotton a toxic and died.");
+            hero.Kill($"You have gotton a toxic and died because You cannot exit this labyrinth before {MAX_PLAY_STEPS}.");
         }
     }
 }
--- MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
+++ MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
@@ -1,4 +1,7 @@
-﻿using MinotaurLabyrinth;
+﻿using System.Reflection;
+using MinotaurLabyrinth;
+using Moq;
+using Moq.Protected;
 
 namespace MinotaurLabyrinthTest
 {
@@ -13,9 +16,99 @@ namespace MinotaurLabyrinthTest
 
 
         [TestMethod]
-        public void Test()
+        public void Test_Create_Toxic_Room_ByType()
         {
-            Assert.AreNotSame(1, 2);
+            Room room = RoomFactory.Instance.BuildRoom(RoomType.Toxic);
+            string className = room.GetType().FullName;
+            Assert.AreEqual(className, "MinotaurLabyrinth.Toxic");
+        }
+
+        [TestMethod]
+        public void Test_PlayGame_Win_When_Dice10_And_Small_True()
+        {
+            var mock = new Mock<Toxic>();
+            mock.Protected().Setup<int>("Dice3Dices").Returns(10);
+            var toxic = mock.Object;
+
+            bool smallFlag = true;
+            MethodInfo playGameMethod = typeof(Toxic).GetMethod("PlayGame", BindingFlags.NonPublic | BindingFlags.Instance);
+            bool result = (bool)playGameMethod.Invoke(toxic, new object[] { smallFlag });
+
+            Assert.AreEqual(result, true);
+        }
+
+        [TestMethod]
+        public void Test_PlayGame_Lost_When_Dice10_And_Small_False()
+        {
+            var mock = new Mock<Toxic>();
+            mock.Protected().Setup<int>("Dice3Dices").Returns(10);
+            var toxic = mock.Object;
+
+            bool smallFlag = false;
+            MethodInfo playGameMethod = typeof(Toxic).GetMethod("PlayGame", BindingFlags.NonPublic | BindingFlags.Instance);
+            bool result = (bool)playGameMethod.Invoke(toxic, new object[] { smallFlag });
+
+            Assert.AreEqual(result, false);
+        }
+
+
+        [TestMethod]
+        public void Test_PlayGame_Lost_When_Dice11_And_Small_True()
+        {
+            var mock = new Mock<Toxic>();
+            mock.Protected().Setup<int>("Dice3Dices").Returns(11);
+            var toxic = mock.Object;
+
+            bool smallFlag = true;
+            MethodInfo playGameMethod = typeof(Toxic).GetMethod("PlayGame", BindingFlags.NonPublic | BindingFlags.Instance);
+            bool result = (bool)playGameMethod.Invoke(toxic, new object[] { smallFlag });
+
+            Assert.AreEqual(result, false);
+        }
+
+        [TestMethod]
+        public void Test_PlayGame_Win_When_Dice11_And_Small_False()
+        {
+            var mock = new Mock<Toxic>();
+            mock.Protected().Setup<int>("Dice3Dices").Returns(11);
+            var toxic = mock.Object;
+            bool smallFlag = false;
+
+            MethodInfo playGameMethod = typeof(Toxic).GetMethod("PlayGame", BindingFlags.NonPublic | BindingFlags.Instance);
+            bool result = (bool)playGameMethod.Invoke(toxic, new object[] { smallFlag });
+
+            Assert.AreEqual(result, true);
+        }
+
+
+        [TestMethod]
+        public void Test_Active_WinGame()
+        {
+            var mock = new Mock<Toxic>() { CallBase = true }; //need to set Callbase = true, then can call the override/virtual method
+            mock.Protected().Setup<int>("Dice3Dices").Returns(11);
+            mock.Protected().Setup<bool>("GetUserSelectOption").Returns(false);
+            var toxic = mock.Object;
+           
+            (Map map, Hero hero) = LabyrinthCreator.InitializeMap(Size.Small);
+            toxic.Activate(hero, map);
+
+            mock.Protected().Verify("HandleWinGame", Times.Once());
+            Assert.AreEqual(hero.IsPoisoned, false);
+        }
+
+        [TestMethod]
+        public void Test_Active_LostGame()
+        {
+            var mock = new Mock<Toxic>() { CallBase = true }; //need to set Callbase = true, then can call the override/virtual method
+            mock.Protected().Setup<int>("Dice3Dices").Returns(11);
+            mock.Protected().Setup<bool>("GetUserSelectOption").Returns(true);
+            var toxic = mock.Object;
+
+            (Map map, Hero hero) = LabyrinthCreator.InitializeMap(Size.Small);
+            toxic.Activate(hero, map);
+
+            mock.Protected().Verify("HandleLostGame", Times.Once(), new object[] { hero });
+            Assert.AreEqual(hero.IsPoisoned, true);
         }
     }
 }
